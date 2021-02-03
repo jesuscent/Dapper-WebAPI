a701f1e11e2beba59c0a3934c103cebc8e0c6f0f
jesuscent <jgcc_maiko@hotmail.com>
2021-02-02 23:22:57 -0500

    base

---
+++ Dapper.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Application.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    
+
+    }
+}
